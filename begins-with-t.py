5a6a29c64f9e64885e78f7e5dae177ef74aedc6d
seanp <seanpatmeade@gmail.com>
2019-03-28 06:30:44 +0000

    Basic solution to Question 2.

---
+++ begins-with-t.py
@@ -0,0 +1,10 @@
+# Import datetime module
+import datetime
+# If weekday is equal to Tuesday (1) or Thursday (3)
+if datetime.datetime.today().weekday() == 1 | datetime.datetime.today().weekday() == 3:
+    # Then print statement 
+    print("Yes - today begins with a T.")
+# Otherwise
+else:
+    # Print statement
+    print("No - today does not begin with T.")
